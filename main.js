d50a71b4d0cada74268c72a95202d755528655d0
Wikaryyy <59520094+Wikaryyy@users.noreply.github.com>
2021-01-18 01:32:58 +0100

    .
    .

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+
 var spellName = document.getElementById("spellName");
 var level = document.getElementById("level");
 var castingTime = document.getElementById("castingTime");
@@ -15,6 +16,67 @@ var controller = new AbortController();
 
 var searchRequest;
 
+//autoComplete.js Library 
+const autoComplete = new autoComplete({
+    name:"dnd spells",
+    data: {                              // Data src [Array, Function, Async] | (REQUIRED)
+      src: async () => {
+        // API key token
+        const token = "this_is_the_API_token_number";
+        // User search query
+        const query = document.querySelector("#autoComplete").value;
+        // Fetch External Data Source
+        const source = await fetch(`https://www.dnd5eapi.co/api/spells/${query}`);
+        // Format data into JSON
+        const data = await source.json();
+        // Return Fetched data
+        return data.spells;
+      },
+      key: ["title"],
+      cache: false
+    },
+    sort: (a, b) => {                    // Sort rendered results ascendingly | (Optional)
+        if (a.match < b.match) return -1;
+        if (a.match > b.match) return 1;
+        return 0;
+    },
+    placeHolder: "Spell...",     // Place Holder text                 | (Optional)
+    selector: "#autoComplete",           // Input field selector              | (Optional)
+    observer: true,                      // Input field observer | (Optional)
+    threshold: 3,                        // Min. Chars length to start Engine | (Optional)
+    debounce: 300,                       // Post duration for engine to start | (Optional)
+    searchEngine: "loose",              // Search Engine type/mode           | (Optional)
+    resultsList: {                       // Rendered results list object      | (Optional)
+        container: source => {
+            source.setAttribute("id", "food_list");
+        },
+        destination: "#autoComplete",
+        position: "afterend",
+        element: "ul"
+    },
+    maxResults: 5,                         // Max. number of rendered results | (Optional)
+    highlight: true,                       // Highlight matching results      | (Optional)
+    resultItem: {                          // Rendered result item            | (Optional)
+        content: (data, source) => {
+            source.innerHTML = data.match;
+        },
+        element: "li"
+    },
+    noResults: (dataFeedback, generateList) => {
+        // Generate autoComplete List
+        generateList(autoCompleteJS, dataFeedback, dataFeedback.results);
+        // No Results List Item
+        const result = document.createElement("li");
+        result.setAttribute("class", "no_result");
+        result.setAttribute("tabindex", "1");
+        result.innerHTML = `<span style="display: flex; align-items: center; font-weight: 100; color: rgba(0,0,0,.2);">Found No Results for "${dataFeedback.query}"</span>`;
+        document.querySelector(`#${autoCompleteJS.resultsList.idName}`).appendChild(result);
+    },
+    onSelection: feedback => {             // Action script onSelection event | (Optional)
+        console.log(feedback.selection.value.image_url);
+    }
+});
+
 function clearSpellContainer() {
     spellName.innerHTML = " ";
     level.innerHTML = " ";
@@ -30,7 +92,7 @@ function clearSpellContainer() {
 
 function getSpell(){
     clearSpellContainer()
-    var searchArea = document.getElementById("searchArea").value.toLowerCase().replaceAll(" ","-");
+    var searchArea = document.getElementById("autoComplete").value.toLowerCase().replaceAll(" ","-");
     searchRequest = "spells/"+searchArea;
     fetch('https://www.dnd5eapi.co/api/'+searchRequest)
         .then(function(resp) {
@@ -107,5 +169,6 @@ function getSpell(){
             }
     })
 }
+
 
 
